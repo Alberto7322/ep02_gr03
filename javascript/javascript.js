37957ae7aab4897b51cc847d129431a24ec15172
Albestia7322 <100429155@alumnos.uc3m.es>
2022-10-14 12:09:12 +0200

    v25

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,20 +1,70 @@
-function crearSesion(){
-    const userInput=document.querySelector("#username");
-    const passInput=document.querySelector("#password");
-    const nameInput=document.querySelector("#name");
-    const mailInput=document.querySelector("#email");
-    const dateInput=document.querySelector("#date");
-    const urlInput=document.querySelector("#url");
-    const checkInput=document.querySelector("#check");
+function openBotonOpciones(){
+    document.getElementById('boton-opciones').style.display = "block";
+}
 
-    alert(userInput.value)
+function closeBotonOpciones(){
+    document.getElementById('boton-opciones').style.display = "none";
+}
+function checkCookie(campo) {
+    console.log(sessionStorage.getItem(campo))
+    if (sessionStorage.getItem(campo) == null) {    
+        return true;
+    } else {
+        return false;
+    }
+}
 
-    var data =JSON.stringify({'user': userInput.value, 
-    'password': passInput.value, 'name': nameInput.value, 
-    'email': mailInput.value, 'date': dateInput.value, 
-    'url':urlInput.value, 'check': checkInput.checked
-    });
+function storeValues(form){
+    console.log(form.email.value)
+    if (checkCookie(form.email.value)){
+        let array = [form.usuario.value, form.psw.value, form.nombreapellidos.value, form.email.value, form.fechadenacimiento.value];
+        var strForm = serializarArray(array);
+        sessionStorage.setItem(form.email.value, strForm);
+        return true;
+    }else{
+        alert("Ya existe una cookie asociada a este correo");
+        return false;
+    }
+}
+function serializarArray(formulario){
+    var strFormulario = "";
+    for (var i=0; i<formulario.length; i++){
+        strFormulario += formulario[i]+",";
+    }
+    strFormulario = strFormulario.substr(0, strFormulario.length-1);
+    return strFormulario;
+}
+var emailiniciado = "";
+var expiniciado = "";
 
-    sessionStorage.setItem("user",data)
- 
+function validarDatos(form){
+    var email = form.email.value;
+    var psw = form.psw.value;
+
+    if (checkCookie(email) == false){
+        if (checkPassword(email, psw)){
+            alert("Has iniciado sesión correctamente");
+            window.location.href = "entrega2.html";
+            console.log(window.location.href)
+            
+            
+          
+            return false;
+        }else{
+            alert("La password no es correcta");
+            return false;
+        }
+    }else{
+        alert("No existe ninguna cookie asociada a ese correo");
+        return false;
+    }
+}
+function checkPassword(email, psw){
+    let cookie = sessionStorage.getItem(email);
+    let array = cookie.split(",");
+    if (array[2] == psw){
+        return true;
+    }else{
+        return false;
+    }
 }
